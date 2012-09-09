dd7add828eca62292a6c7206c187dc3b90aa17aa
Salma <salma@salma-laptop.(none)>
2012-09-09 15:07:40 -0400

    fourth commit

--- strops/strlen.c
+++
@@ -1,8 +0,0 @@
-int mystrlen(char *str)
-{
-    int count;
-    count = 0;
-    while (str[++count] != '\0')
-        ;
-    return count;
-}
--- strops/strlen.h
+++
@@ -1,4 +0,0 @@
-#ifndef MY_STRLEN
-#define MY_STRLEN
-int mystrlen(char *);
-#endif
